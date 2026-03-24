3e77cda99c2e00fe46d7e6a11544f2e5fad82472
kdef2012 <kdef2012@gmail.com>
2026-03-24 10:30:21 -0400

    Add files via upload

--- krown-academy/app/layout.tsx
+++ krown-academy/app/layout.tsx
@@ -1,7 +1,5 @@
 import type { Metadata } from "next";
 import "./globals.css";
-import Navbar from "@/components/Navbar";
-import Footer from "@/components/Footer";
 
 export const metadata: Metadata = {
   title: "Krown Academy | Earn Your Krown",
@@ -35,9 +33,7 @@ export default function RootLayout({
         />
       </head>
       <body className="bg-krown-white">
-        <Navbar />
         <main>{children}</main>
-        <Footer />
       </body>
     </html>
   );
